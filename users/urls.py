9a9befd8f059758fdf743192d82bd7d3dc1173bf
Hugo Belém <belem.hugo@hotmail.com>
2024-09-25 13:25:29 -0300

    building login & profile

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'users'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
     path('profile/', views.profile, name='profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -4,5 +4,8 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+def login(request):
+    return render(request, 'users/login.html')
+
 def profile(request):
     return render(request, 'users/profile.html')
