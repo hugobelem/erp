869822e862131828d84586fde4ad94423b9c2ca0
Hugo Belém <belem.hugo@hotmail.com>
2024-09-26 14:08:13 -0300

    creaded new signals file

---
+++ users/signals.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+
+from .models import Empresa
+
+@receiver(post_save, sender=User)
+def create_empresa(sender, instance, created, **kwargs):
+    if created:
+        user = instance
+        empresas = Empresa.objects.create(user=user)
+
+@receiver(post_delete, sender=User)
+def delete_user(sender, instance, **kwargs):
+    user = instance.user
+    user.delete()
