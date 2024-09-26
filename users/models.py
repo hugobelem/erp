0acfdb6f6df4672731b59ec90e2e2577aee888ab
Hugo Belém <belem.hugo@hotmail.com>
2024-09-26 14:02:08 -0300

    removed signals

--- users/models.py
+++ users/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-from django.dispatch import receiver
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save, post_delete
 
 
 class Empresa(models.Model):
