0de84c23a97ed29304aacbdedab315763d7c0bcd
Hugo Belém <belem.hugo@hotmail.com>
2024-09-27 18:24:07 -0300

    updated CustomUseCreationForm

--- users/forms.py
+++ users/forms.py
@@ -1,20 +1,23 @@
-from django.forms import ModelForm
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import User
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['first_name', 'email', 'username', 'password1', 'password2']
+        fields = [
+            'email',
+            'name',
+            'username',
+        ]
         labels = {
-            'first_name': 'nome',
             'email': 'e-mail',
+            'name': 'nome',
+            'username': 'usuário',
         }
 
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
-        self.fields['password1'].label = 'senha'
         self.fields['password2'].label = 'confirmar senha'
 
         for name, field in self.fields.items():
