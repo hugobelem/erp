e3e374c2180a897eb74deeabe02e21af1f233587
Hugo Belém <belem.hugo@hotmail.com>
2024-09-27 23:47:21 -0300

    added Custom UserChangeForm

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import User
 
 
@@ -24,3 +24,28 @@ class CustomUserCreationForm(UserCreationForm):
                 'autofocus': 'false',
                 'placeholder': name,
             })
+
+class CustomUserChangeForm(UserChangeForm):
+    password = None
+    class Meta:
+        model = User
+        fields = [
+            'email',
+            'name',
+            'username'
+        ]
+        labels = {
+            'email': 'e-mail',
+            'name': 'nome'
+        }
+    
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserChangeForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'peer h-8 w-64 border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm',
+                'autofocus': 'false',
+                'placeholder': name,
+            })    
