16d55b12b9a375e554ec83582668f22a20323f66
Hugo Belém <belem.hugo@hotmail.com>
2024-09-28 01:00:39 -0300

    old logo delete on change

--- users/signals.py
+++ users/signals.py
@@ -19,3 +19,19 @@ def auto_delete_logo_on_delete(sender, instance, **kwargs):
     if logo:
         if os.path.isfile(instance.logo.path):
             os.remove(instance.logo.path)
+
+
+@receiver(pre_save, sender=Empresa)
+def auto_delete_logo_on_change(sender, instance, **kwargs):
+    if not instance.pk:
+        return False
+    
+    try:
+        old_logo = Empresa.objects.get(pk=instance.pk).logo
+    except Empresa.DoesNotExist:
+        return False
+    
+    new_logo = instance.logo
+    if not old_logo == new_logo:
+        if os.path.isfile(old_logo.path):
+            os.remove(old_logo.path)
