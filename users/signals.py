7e31fdc70a0d7e441ed7d538bf9660100772fc45
Hugo Belém <belem.hugo@hotmail.com>
2024-09-28 00:50:53 -0300

    delete logo on delete

--- users/signals.py
+++ users/signals.py
@@ -1,5 +1,8 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save, post_delete, pre_save
 from django.dispatch import receiver
+from django.conf import settings
+
+import os
 
 from .models import Empresa, User
 
@@ -8,3 +11,11 @@ def create_empresa(sender, instance, created, **kwargs):
     if created:
         user = instance
         empresas = Empresa.objects.create(user=user)
+
+
+@receiver(post_delete, sender=Empresa)
+def auto_delete_logo_on_delete(sender, instance, **kwargs):
+    logo = instance.logo
+    if logo:
+        if os.path.isfile(instance.logo.path):
+            os.remove(instance.logo.path)
