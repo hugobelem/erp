72ba53477d3b6ba4ee8d85440771142b92241663
Hugo Belém <belem.hugo@hotmail.com>
2024-09-29 10:50:44 -0300

    rearranged imports

--- erp/urls.py
+++ erp/urls.py
@@ -1,11 +1,12 @@
+from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
-from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
-from django.contrib.auth import views as auth_views
 from users import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
