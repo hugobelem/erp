4b1e1c9d61497b2040973de4f1faaf30658c4c68
Hugo Belém <belem.hugo@hotmail.com>
2024-09-29 22:30:47 -0300

    added empresa views

--- business/views.py
+++ business/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
 
-# Create your views here.
+from .models import Business
+
+from static.assets import avatar
+
+
+def navbar(request):
+    user = request.user
+
+    if request.user.is_authenticated:
+        try:
+            business = Business.objects.filter(user=user).first()
+        except Business.DoesNotExist:
+            business = None
+
+        svg = avatar.generate(request.user.name)
+        first_name = request.user.name.split()[0]
+
+        context = {
+            'first_name': first_name,
+            'avatar': mark_safe(svg),
+            'business': business
+        }
+    
+    return context
+
+
+def empresa(request):
+    context = {}
+    context.update(navbar(request))
+
+    return render(request, 'business/pages/empresa.html', context)
+
