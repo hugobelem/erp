9a751caa8407214191b2f2e5b0cc48a3ac8b1e22
Hugo Belém <belem.hugo@hotmail.com>
2024-09-29 22:50:05 -0300

    added / on urls

--- users/urls.py
+++ users/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('sair/', views.signout, name='logout'),
 
     path('usuario/', views.user, name='user'),
-    path('usuario/editar', views.update_user, name='update_user'),
-    path('usuario/exluir', views.delete_user, name='delete_user'),
+    path('usuario/editar/', views.update_user, name='update_user'),
+    path('usuario/exluir/', views.delete_user, name='delete_user'),
 ]
