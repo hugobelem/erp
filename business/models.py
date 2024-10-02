1ec13a8b4f3f8279ee3b441b4ae673c360e2a21e
Hugo Belém <belem.hugo@hotmail.com>
2024-10-01 22:12:31 -0300

    adeed models

--- business/models.py
+++ business/models.py
@@ -24,3 +24,52 @@ class Business(models.Model):
             return self.fantasia
         else:
             return f'Empresa {self.id}'
+        
+
+class Customer(models.Model):
+    nome = models.CharField(max_length=255, blank=True, null=True)
+    apelido = models.CharField(max_length=255, blank=True, null=True)
+    código = models.CharField(max_length=255, null=True, blank=True)
+    cpf = models.IntegerField(blank=True, null=True)
+    cep = models.IntegerField(blank=True, null=True)
+    cidade = models.CharField(max_length=255, blank=True, null=True)
+    up = models.CharField(max_length=2, blank=True, null=True)
+    endereco = models.CharField(max_length=255, blank=True, null=True)
+    numero = models.IntegerField(blank=True, null=True)
+    bairro = models.CharField(max_length=255, blank=True, null=True)
+    complemento = models.CharField(max_length=255, blank=True, null=True)
+    celular = models.IntegerField(blank=True, null=True)
+    nascimento = models.DateField(blank=True, null=True)
+    sexo = models.CharField(max_length=50, blank=True, null=True)
+    observacoes = models.TextField(blank=True, null=True)
+
+
+class Product(models.Model):
+    descricao = models.CharField(max_length=255, blank=True, null=True)
+    código = models.CharField(max_length=255, null=True, blank=True)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    unidade = models.CharField(max_length=25, blank=True, null=True)
+    estoque = models.IntegerField(blank=True, null=True)
+    marca = models.CharField(max_length=255, blank=True, null=True)
+    custo = models.IntegerField(blank=True, null=True)
+    criado = models.DateTimeField(auto_now_add=True)
+
+
+class Order(models.Model):
+    numero = models.IntegerField(unique=True)
+    produto = models.ManyToManyField(Product, through='OrderItem')
+    cliente = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    itens = models.PositiveIntegerField()
+    desconto = models.IntegerField()
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    criado = models.DateTimeField(auto_now_add=True)
+    recebimento = models.CharField(max_length=255, blank=True, null=True)
+    categoria = models.CharField(max_length=255, blank=True, null=True)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    produto = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantidade = models.PositiveIntegerField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    desconto = models.IntegerField()
+    preco_total = models.DecimalField(max_digits=10, decimal_places=2)
