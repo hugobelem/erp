56d202371d39ebe86e60ac5b8fccf1d118458db3
Hugo Belém <belem.hugo@hotmail.com>
2024-10-01 22:13:27 -0300

    added models on admin

--- business/admin.py
+++ business/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import Business
+from .models import Business, Product, Order, OrderItem
 
 admin.site.register(Business)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
