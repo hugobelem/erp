0b009c3bacae168c0d3aac7d2f3df85a8a885819
Hugo Belém <belem.hugo@hotmail.com>
2024-10-01 23:34:51 -0300

    deleted models

--- business/admin.py
+++ business/admin.py
@@ -1,16 +1,5 @@
 from django.contrib import admin
 
-from .models import Empresa, Cliente, Produto, Pedido, PedidoItem
-
-
-class PedidoItemInline(admin.TabularInline):
-    model = PedidoItem
-    extra = 0
-
-class PedidoAdmin(admin.ModelAdmin):
-    inlines = [PedidoItemInline]
+from .models import Empresa
 
 admin.site.register(Empresa)
-admin.site.register(Cliente)
-admin.site.register(Produto)
-admin.site.register(Pedido, PedidoAdmin)
